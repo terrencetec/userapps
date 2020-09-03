7396c162250778a3416ba7010eb94bacdf10d8ff
k1ctr0 <miyo@icrr.u-tokyo.ac.jp>
2020-09-03 13:10:31 +0900

    Update DAMPMODE_LIST. Add No.17 mode.

--- k1/guardian/VIS_ETMX.py
+++ k1/guardian/VIS_ETMX.py
@@ -1,5 +1,5 @@
 from GRD_VIS import *
 
-DAMPMODE_LIST = [13,14] # MOD-ITMX_MODE%d_SERVO_GAIN will be engaged.
+DAMPMODE_LIST = [13,14,15,16,17] # MOD-ITMX_MODE%d_SERVO_GAIN will be engaged.
 GASDAMP_LIST = ['M5'] #MOD-ITMX_DAMP_GAS_%s_INMON will be engaged
 OPLEV_SUM_THR = 20000 #Threshold  for TMLEN oplev SUM to check oplev is in range or not.
