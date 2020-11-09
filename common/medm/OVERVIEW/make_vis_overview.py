e13464e5a86a88b9f6f4f46121a72ded83e47c72
k1ctr0 <miyo@icrr.u-tokyo.ac.jp>
2020-11-09 12:53:36 +0900

    medm: update the design of the VIS_OVERVIEW.adl

--- common/medm/OVERVIEW/make_vis_overview.py
+++ common/medm/OVERVIEW/make_vis_overview.py
@@ -148,9 +148,32 @@ def gds_mini(x,y,fec='123',subsys='ETMXT'):
     return txt,width,height
 
 
-def usr_mini(x,y,fec='123',system='ETMX'):
-    width = 290
-    height = 15
+def user_mini(x,y,fec='123',system='ETMX',suffix='TOWER_OVERVIEW'):
+    width = 400
+    height = 25
+    sustype = sus_type_is(system).lower()
+    SUSTYPE = sustype.upper()
+    optic = system.lower()
+    OPTIC = system.upper()
+    if sustype == 'typea':
+        macroname = '{common}/medm/macro/vis{optic}t_overview_macro.txt'.format(common=common,optic=optic)
+        ctrladl = '{common}/medm/{sustype}/VIS_{SUSTYPE}_TOWER_OVERVIEW.adl'.format(common=common,optic=optic,SUSTYPE=SUSTYPE,sustype=sustype)
+    elif sustype=='typeb':
+        macroname = '{common}/medm/macro/vis{optic}_overview_macro.txt'.format(common=common,optic=optic)
+        ctrladl = '{common}/medm/{sustype}/VIS_CUST_{SUSTYPE}_OVERVIEW.adl'.format(common=common,optic=optic,SUSTYPE=SUSTYPE,sustype=sustype)
+    elif sustype=='typebp':
+        macroname = '{common}/medm/macro/vis{optic}_overview_macro.txt'.format(common=common,optic=optic)
+        ctrladl = '{common}/medm/{sustype}/VIS_{SUSTYPE}_OVERVIEW.adl'.format(common=common,optic=optic,SUSTYPE=SUSTYPE,sustype=sustype)
+    elif sustype=='typec':
+        macroname = '{common}/medm/macro/vis{optic}_overview_macro.txt'.format(common=common,optic=optic)
+        ctrladl = '{common}/medm/{sustype}/VIS_CUST_{SUSTYPE}_OVERVIEW.adl'.format(common=common,optic=optic,SUSTYPE=SUSTYPE,sustype=sustype)
+    elif sustype=='typetms':
+        macroname = '{common}/medm/macro/vis{optic}_overview_macro.txt'.format(common=common,optic=optic)
+        ctrladl = '{common}/medm/TMS/VIS_TMS_OVERVIEW.adl'.format(common=common)          
+    else:
+        raise ValueError('!')
+
+    print(macroname)
     txt = '''
     composite {{
     object {{
@@ -160,62 +183,106 @@ def usr_mini(x,y,fec='123',system='ETMX'):
     height=15
     }}
     "composite name"=""
-    "composite file"="{common}/medm/OVERVIEW/MINI/USER_MINI.adl;FEC={fec},SYSTEM={system}"
+    "composite file"="{common}/medm/OVERVIEW/MINI/USER_MINI.adl;FEC={fec},SYSTEM={system},macroname={macroname},ctrladl={ctrladl}"
     }}
-    '''.format(common=common,x=x,y=y,fec=fec,system=system)
+    '''.format(common=common,x=x,y=y,fec=fec,system=system,sustype=sustype,suffix=suffix,SUSTYPE=SUSTYPE,macroname=macroname,ctrladl=ctrladl)
     return txt,width,height
 
+def trip_mini(x,y,optic='ETMX'):
+    width = 150
+    height = 25
+    sus_type = sus_type_is(optic)
+    txt = '''
+    composite {{
+    object {{
+    x={x}
+    y={y}
+    width=148
+    height=30
+    }}
+    "composite name"=""
+    "composite file"="{mini_name};OPTIC={optic}"
+    }}
+    '''.format(common=common,x=x,y=y,mini_name=trip_mini_name_is(optic),optic=optic)
+    return txt,width,height
 
 
 if __name__=='__main__':
     systems = ['ETMX','ETMY','ITMX','ITMY','BS','SRM','SR2','SR3','PRM',
                'PR2','PR3','MCI','MCE','MCO','IMMT1','IMMT2','OMMT1','OMMT2',
                'OSTM','TMSX','TMSY']
-    models = {'ETMX':[['VISETMXT',68],['VISETMXP',69],['VISETMXMON',79],['MODALETMX',86]],
-              'ETMY':[['VISETMYT',65],['VISETMYP',64],['VISETMYMON',80],['MODALETMY',89]],
-              'ITMX':[['VISITMXT',66],['VISITMXP',67],['VISITMXMON',77],['MODALITMX',70]],
-              'ITMY':[['VISITMYT',62],['VISITMYP',63],['VISITMYMON',78],['MODALITMY',11]],
-              'BS':[['VISBST',93],['VISBSP',92]],
-              'SRM':[['VISSRMT',58],['VISSRMP',59]],
-              'SR2':[['VISSR2T',54],['VISSR2P',55]],
-              'SR3':[['VISSR3T',56],['VISSR3P',57]],
-              'PRM':[['VISPRM',24]],
-              'PR2':[['VISPR2',91]],
-              'PR3':[['VISPR3',23]],
-              'MCI':[['VISMCI',34]],
-              'MCE':[['VISMCE',35]],
-              'MCO':[['VISMCO',36]],
-              'IMMT1':[['VISIMMT1',81]],
-              'IMMT2':[['VISIMMT2',82]],
-              'OMMT1':[['VISOMMT1',83]],
-              'OMMT2':[['VISOMMT2',84]],
-              'OSTM':[['VISOSTM',87]],
-              'TMSX':[['VISTMSX',107]],
-              'TMSY':[['VISTMSY',114]],              
-    }
     
+    models = {'ETMX':[['VISETMXT',102],['VISETMXP',103],['VISETMXMON',104],['MODALETMX',105]],
+              'ETMY':[['VISETMYT',107],['VISETMYP',108],['VISETMYMON',109],['MODALETMY',110]],
+              'ITMX':[['VISITMXT',92],['VISITMXP',93],['VISITMXMON',94],['MODALITMX',95]],
+              'ITMY':[['VISITMYT',97],['VISITMYP',98],['VISITMYMON',99],['MODALITMY',100]],
+              'BS':[['VISBST',60],['VISBSP',61]],
+              'SRM':[['VISSRMT',75],['VISSRMP',76]],
+              'SR2':[['VISSR2T',65],['VISSR2P',66]],
+              'SR3':[['VISSR3T',70],['VISSR3P',71]],
+              'PRM':[['VISPRM',55]],
+              'PR2':[['VISPR2',45]],
+              'PR3':[['VISPR3',50]],
+              'MCI':[['VISMCI',38]],
+              'MCE':[['VISMCE',39]],
+              'MCO':[['VISMCO',40]],
+              'IMMT1':[['VISIMMT1',42]],
+              'IMMT2':[['VISIMMT2',43]],
+              'OMMT1':[['VISOMMT1',80]],
+              'OMMT2':[['VISOMMT2',81]],
+              'OSTM':[['VISOSTM',82]],
+              'TMSX':[['VISTMSX',117]],
+              'TMSY':[['VISTMSY',122]],              
+    }
+    sus_types = {'TypeA':['ETMX','ETMY','ITMX','ITMY'],
+                'TypeB':['BS','SR2','SR3','SRM'],
+                'TypeBp':['PR2','PR3','PRM'],
+                'TypeC':['MCI','MCO','MCE','IMMT1','IMMT2','OSTM','OMMT1','OMMT2'],
+                'TypeTMS':['TMSX','TMSY']}
+    
+    def sus_type_is(optic):
+        for sus_type in ['TypeA','TypeB','TypeBp','TypeC','TypeTMS']:
+            if optic in sus_types[sus_type]:
+                return sus_type
+            else:
+                pass
+        return None
+    
+    def trip_mini_name_is(optic):
+        prefix = '/users/Commissioning/medm/'
+        sus_type = sus_type_is(optic)
+        if sus_type == 'TypeA':
+            adl = 'tripped_microA.adl'
+        elif sus_type == 'TypeB':
+            adl = 'tripped_microB.adl'
+        elif sus_type == 'TypeBp':
+            adl = 'tripped_microBp.adl'            
+        elif sus_type == 'TypeC':
+            adl = 'tripped_microC.adl'            
+        elif sus_type == 'TypeTMS':
+            adl = 'tripped_microT.adl'            
+        else:
+            raise ValueError('!')        
+        return prefix+adl
+    
+        
     height = 10
     width = 10
     contents = header    
     with open('./MINI/VIS_MINI.adl','w') as f:
         for system in systems:
             model,fec = models[system][0]
-            txt,w1,h1 = usr_mini(x=width,y=height,system=system)
+            txt,w1,h1 = user_mini(x=width,y=height,system=system)
             contents += txt            
             width += w1+2
             txt,w2,h = grd_mini(x=width,y=height,system='VIS_'+system)
             contents += txt
             width += w2+2
-            # if system in ['ETMX','ETMY','ITMX','ITMY']:
-            #     txt,w3,h = grd_mini(x=width,y=height,system='QUA_'+system)
-            #     contents += txt                
-            #     txt,w3,h = grd_mini(x=width,y=height+(h+2),system='NEW_'+system)
-            #     contents += txt                
-            #     width += w3+2
-            #     _w = w1+w2+w3+6
-            # else:
-            #     width += w2+2
             _w = w1+w2+4
+            txt,w3,h = trip_mini(x=width,y=height,optic=system)
+            contents += txt
+            width += w3+2
+            _w = w1+w2+w3+6            
             for model in models[system]:
                 model,fec = model
                 txt,w,h = sdf_mini(x=width,y=height,fec=fec,subsys=model)
@@ -224,7 +291,7 @@ if __name__=='__main__':
                 contents += txt                
                 height += h+2
             if len(models[system])==1:
-                height += h
+                height += h                
             width -= _w
             
         f.write(contents)    
