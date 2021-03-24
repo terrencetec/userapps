bb69f64cb2599b12f5659bc741984b6e7a98e81b
k1ctr0 <miyo@icrr.u-tokyo.ac.jp>
2021-03-24 16:58:48 +0900

    oplev: move to here from awsome.

--- common/scripts/autocommissioning/oplev/oplev.py
+++ common/scripts/autocommissioning/oplev/oplev.py
@@ -15,14 +15,14 @@ def plot(output):
     df = [i.split(',') for i in df]
     df = pd.read_csv(output,header=0)
     
-    _name = ['K1:VIS-MCO_TM_OPLEV_TILT_SEG1_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_SEG2_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_SEG3_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_SEG4_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_PIT_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_YAW_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_SUM_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_CRS_INMON']
+    _name = ['K1:VIS-MCE_TM_OPLEV_TILT_SEG1_INMON',
+             'K1:VIS-MCE_TM_OPLEV_TILT_SEG2_INMON',
+             'K1:VIS-MCE_TM_OPLEV_TILT_SEG3_INMON',
+             'K1:VIS-MCE_TM_OPLEV_TILT_SEG4_INMON',
+             'K1:VIS-MCE_TM_OPLEV_TILT_PIT_INMON',
+             'K1:VIS-MCE_TM_OPLEV_TILT_YAW_INMON',
+             'K1:VIS-MCE_TM_OPLEV_TILT_SUM_INMON',
+             'K1:VIS-MCE_TM_OPLEV_TILT_CRS_INMON']
     
     col,row = 3,3
     fig, ax = plt.subplots(col,row,figsize=(10,4),sharex=True)
@@ -63,6 +63,12 @@ def plot(output):
     plt.close()    
 
 def calibration(output):
+    '''
+    '''
+    optic, dof = output.replace('.txt','').split('_')
+    optic = optic.upper()
+    dof = dof.upper()
+    print(optic,dof)
     f = open(output,'r',encoding='UTF-8')
     df = f.readlines()
     f.close()
@@ -70,18 +76,15 @@ def calibration(output):
     df = pd.read_csv(output,header=0)
     df = df.sort_values('Memo')
     
-    _name = [#'K1:VIS-MCO_TM_OPLEV_TILT_SEG1_INMON',
-             #'K1:VIS-MCO_TM_OPLEV_TILT_SEG2_INMON',
-             #'K1:VIS-MCO_TM_OPLEV_TILT_SEG3_INMON',
-             #'K1:VIS-MCO_TM_OPLEV_TILT_SEG4_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_PIT_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_YAW_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_SUM_INMON',
-             'K1:VIS-MCO_TM_OPLEV_TILT_CRS_INMON']
+    _name = ['K1:VIS-{0}_TM_OPLEV_TILT_PIT_INMON'.format(optic),
+             'K1:VIS-{0}_TM_OPLEV_TILT_YAW_INMON'.format(optic),
+             'K1:VIS-{0}_TM_OPLEV_TILT_SUM_INMON'.format(optic),
+             'K1:VIS-{0}_TM_OPLEV_TILT_CRS_INMON'.format(optic)]
     
     col,row = 4,1
     fig, ax = plt.subplots(col,row,figsize=(8,8),sharex=True)
-    fig.subplots_adjust(wspace=0.1, hspace=0.05, left=0.15, right=0.6)    
+    fig.subplots_adjust(wspace=0.1, hspace=0.05, left=0.15, right=0.6, top=0.92)
+    plt.suptitle('OpLev calibration of {0} {1}'.format(optic,dof),fontsize=20)
     for i,_ax in enumerate(ax):
         if i==3:
             _ax.set_xlabel('Distance [mm]')
@@ -92,28 +95,29 @@ def calibration(output):
         ylabel = 'Value [count]'
         _ax.set_ylabel(ylabel)
         
-        if n<len(_name):                
+        if n<len(_name):
             _ax.errorbar(x=df['Memo'],
                          y=df[_name[n]+'.mean'],
                          yerr=df[_name[n]+'.std'],
                          fmt='ko',label=_name[n],
                          markersize=2,capsize=3)
             #
-            _df = df[df['Memo']>4]
-            _df = _df[_df['Memo']<11]            
-            if 'PIT' in _name[n]:
-                func = lambda x,a,b: a+b*x
-                x = _df['Memo']
-                popt, pcov = curve_fit(func,x,_df[_name[n]+'.mean'])
-                _x = np.linspace(int(x.min()),int(x.max())+1,100)
-                a,b = popt
-                label = '{0:3.2f}*x + {1:3.2f}'.format(b,a)
-                _ax.plot(_x,func(_x,*popt),label=label)
-            if 'YAW' in _name[n]:
+            _df = df[df['Memo']>=df['Memo'].min()+1]
+            _df = _df[_df['Memo']<=_df['Memo'].max()-1.5]
+            if dof not in _name[n]:
+                if 'SUM' not in _name[n] and 'CRS' not in _name[n]:
+                    func = lambda x,a,b: a+b*x
+                    x = _df['Memo']
+                    popt, pcov = curve_fit(func,x,_df[_name[n]+'.mean'])
+                    _x = np.linspace(int(x.min()),int(x.max())+1,100)
+                    a,b = popt
+                    label = '{0:3.2f}*x + {1:3.2f}'.format(b,a)
+                    _ax.plot(_x,func(_x,*popt),label=label)
+            if dof in _name[n]:
                 func = lambda x,a,b,c,d: a*scipy.special.erf(b*(x-c)) + d
                 x = _df['Memo']
-                bounds = [[-2, -2, -10, -2],
-                          [+2, +2, +10, +2]]
+                bounds = [[-1, -5, -20, -2],
+                          [+1, +5, +20, +2]]
                 popt, pcov = curve_fit(func,x,_df[_name[n]+'.mean'],bounds=bounds)
                 a,b,c,d = popt
                 _x = np.linspace(int(x.min()),int(x.max())+1,100)
@@ -123,7 +127,7 @@ def calibration(output):
                 slope_inv = a*b*2/np.sqrt(np.pi)
                 label = '{0:3.2f}*(x-{1:3.2f})'.format(slope_inv,c)
                 _ax.plot(_x,func(_x,a,b,c),label=label)
-                print('Slope value is {0:3.2f} [mm/count].'.format(1/slope_inv))
+
                 sigma = 1/b*np.sqrt(2)
                 linrange = sigma/2 #?
                 print('Beam radius is {0:3.2f} [mm]?.'.format(sigma))
@@ -135,7 +139,7 @@ def calibration(output):
                 func = lambda x,a,b: a+b*x
                 x = _df['Memo']
                 popt, pcov = curve_fit(func,x,_df[_name[n]+'.mean'])
-                _x = np.linspace(int(x.min()),int(x.max())+1,100)
+                _x = np.linspace(int(x.min()),int(x.max())+1,100)            
                 a,b = popt
                 label = '{0:3.2f}*x + {1:3.2f}'.format(b,a)
                 _ax.plot(_x,func(_x,*popt),label=label)   
@@ -143,7 +147,7 @@ def calibration(output):
             if 'SEG' in _name[n]:
                 _ax.set_ylim(-10000,0)
             elif 'SUM' in _name[n]:
-                _ax.set_ylim(0,16000)                    
+                _ax.set_ylim(0,20000)                    
             elif 'CRS' in _name[n]:
                 _ax.set_ylim(-5000,5000)
             else:
@@ -153,12 +157,15 @@ def calibration(output):
         else:
             break
     #
-    #plt.tight_layout()
-    plt.show()
-    plt.savefig('calib.png')
+    if True:
+        plt.show()
+    else:
+        fname = output.replace('.txt','.png')
+        plt.savefig(fname)
     plt.close()            
     
-
 if __name__=='__main__':
     #plot('./mco_yaw.txt')
-    calibration('./mco_yaw.txt')
+    calibration('mco_yaw.txt')    
+    calibration('mce_yaw.txt')
+    calibration('mce_pit.txt')
